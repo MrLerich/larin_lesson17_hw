d73ce6b89fb855aa6225acbf57ccc0df9b49352c
MrLerich <93153582+MrLerich@users.noreply.github.com>
2022-07-17 23:12:23 +0300

    доделал основное задание

--- app.py
+++ app.py
@@ -1,44 +1,54 @@
 # app.py
 
-from flask import Flask, request
-from flask_restx import Api, Resource
+from flask import Flask, request, jsonify
+from flask_restx import Api, Resource, Namespace
+
+import models
 from models import *
-from config import app
+from config import *
 
-api = Api(app)
+api: Api = Api(app, description='Movies API')
 
-movies_ns = api.namespace('movies')
-directors_ns = api.namespace('directors')
-genre_ns = api.namespace('genre')
+# Создаю namespaces
+movies_ns: Namespace = api.namespace('movies')
+directors_ns: Namespace = api.namespace('directors')
+genre_ns: Namespace = api.namespace('genre')
 
-# Возвращает список всех фильмов наше БД
+
 @movies_ns.route('/')
-class MovieView(Resource):
+class MoviesView(Resource):
+
     def get(self):
+        movies = db.session.query(models.Movie).all()
         director_id = request.args.get('director_id')
         genre_id = request.args.get('genre_id')
         # представление возвращает только фильмы с определенным режиссером и жанром по запросу типа: /movies/?director_id=2&genre_id=4
         if director_id and genre_id:
             query = movies_schema.dump(Movie.query.filter(Movie.director_id == director_id,
-                                                           Movie.genre_id == genre_id).all())
+                                                          Movie.genre_id == genre_id).all())
             return query, 200
 
         if director_id:
-            query = movies_schema.dump(Movie.query.filter(Movie.director_id == director_id).all())
-            return query, 200
+            director = movies_schema.dump(Movie.query.filter(Movie.director_id == director_id).all())
+            return director, 200
 
         if genre_id:
-            query = movies_schema.dump(Movie.query.filter(Movie.genre_id == genre_id).all())
-            return query, 200
+            genre = movies_schema.dump(Movie.query.filter(Movie.genre_id == genre_id).all())
+            return genre, 200
 
-@movies_ns.route('/<int:pk>')
+        else:
+            return movies_schema.dump(movies), 200
+
+
+@movies_ns.route('/<int:movie_id>')
 class MovieView(Resource):
-    def get(self, pk: int):
-        return movies_schema.dump(Movie.query.get_or_404(pk)), 200
+    def get(self, movie_id: int):
+        movie = db.session.query(models.Movie).filter(models.Movie.id == movie_id).first()
 
+        if movie is None:
+            return f'Фильма с идентификатором №{movie_id} нет в базе', 404
 
-
-
+        return movie_schema.dump(movie), 200
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,10 +1,11 @@
-from marshmallow import Schema,fields
+from marshmallow import Schema, fields
 from config import db
 
 
 # Модель фильма
 class Movie(db.Model):
     __tablename__ = 'movie'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
     description = db.Column(db.String(255))
@@ -12,52 +13,57 @@ class Movie(db.Model):
     year = db.Column(db.Integer)
     rating = db.Column(db.Float)
     genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
-    genre = db.relationship("Genre")
     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
+
+    genre = db.relationship("Genre")
     director = db.relationship("Director")
+
 
 # Модель Жанра
 class Director(db.Model):
     __tablename__ = 'director'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
+
 
 # Модель Режисера
 class Genre(db.Model):
     __tablename__ = 'genre'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
 
 
 # Схема для получения полей сложной структуры
 class DirectorSchema(Schema):
-    id = fields.Int()
+    id = fields.Int(dump_only=True)
     name = fields.Str()
 
 
 class GenreSchema(Schema):
-    id = fields.Int()
+    id = fields.Int(dump_only=True)
     name = fields.Str()
 
 
 class MovieSchema(Schema):
-    id = fields.Int()
+    id = fields.Int(dump_only=True)
     title = fields.Str()
     description = fields.Str()
     trailer = fields.Str()
     year = fields.Int()
     rating = fields.Float()
 
-    genre = fields.Pluck('GenreSchema','name')
-    director = fields.Pluck('DirectorSchema','name')
+    genre = fields.Pluck('GenreSchema', 'name')
+    director = fields.Pluck('DirectorSchema', 'name')
+
 
 # Создам экземпляры схемы
-movie_schema = MovieSchema() #для одного поля
-movies_schema = MovieSchema(many=True) # для нескольких полей
+movie_schema = MovieSchema()  # для одного поля
+movies_schema = MovieSchema(many=True)  # для нескольких полей
 
-director_schema = DirectorSchema() #для одного поля
-directors_schema = DirectorSchema(many=True) # для нескольких полей
+director_schema = DirectorSchema()  # для одного поля
+directors_schema = DirectorSchema(many=True)  # для нескольких полей
 
 genre_schema = GenreSchema()
-genres_schema = GenreSchema(many=True) # для нескольких полей
-
+genres_schema = GenreSchema(many=True)  # для нескольких полей
